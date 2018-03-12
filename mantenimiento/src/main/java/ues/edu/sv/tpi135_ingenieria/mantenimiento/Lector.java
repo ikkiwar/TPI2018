7c6cab3d15a2bc033ee786c43fd4a07517279935
esperanza <da13002@ues.edu.sv>
2018-03-12 10:37:24 -0600

    Metodo POST terminado con su respectiva prueba

--- mantenimiento/src/main/java/ues/edu/sv/tpi135_ingenieria/mantenimiento/Lector.java
+++ mantenimiento/src/main/java/ues/edu/sv/tpi135_ingenieria/mantenimiento/Lector.java
@@ -54,7 +54,7 @@ public class Lector implements Serializable {
     public List<String> obtenerArchivos(final String path) {
         List<String> listaDeArchivos = null;
 
-        try (Stream<Path> paths = Files.walk(Paths.get(path))) { // devuelve una coleccion de las rutas de los archivo .csv
+        /*try (Stream<Path> paths = Files.walk(Paths.get(path))) { // devuelve una coleccion de las rutas de los archivo .csv
             listaDeArchivos = paths.map(a -> {
                 if (Files.isRegularFile(a) && a.toString().endsWith(".csv")) {
                     return a.toString();
@@ -71,8 +71,13 @@ public class Lector implements Serializable {
         } catch (IOException ex) {
             Logger.getLogger(Lector.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-     
+        */
+        try {
+            Files.walk(Paths.get(path)).filter(a -> a.toFile().getName().endsWith(".csv"))
+                    .forEach(f -> listaDeArchivos.add(f.toUri().getPath()));
+        } catch (IOException ex) {
+            Logger.getLogger(Lector.class.getName()).log(Level.SEVERE, null, ex);
+        } 
         return listaDeArchivos;
     }
 
@@ -106,7 +111,6 @@ public class Lector implements Serializable {
 
             System.out.println(separado);
         }
-
         return objetos;
     }
 
