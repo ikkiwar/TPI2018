cc1c4cd6d66cee14c90602fe310146fa6e89979e
ikkiwar <rodriguezkev07@gmail.com>
2018-03-15 02:01:27 -0600

    pruebas unitarias de la calse lector listas

--- mantenimiento/src/main/java/ues/edu/sv/tpi135_ingenieria/mantenimiento/Lector.java
+++ mantenimiento/src/main/java/ues/edu/sv/tpi135_ingenieria/mantenimiento/Lector.java
@@ -23,81 +23,75 @@ import java.util.stream.Stream;
  *
  * @author kevin
  */
-public class Lector implements Serializable {
-    
+public class Lector {
+
     private static final Lector instancia = new Lector();
-    
-    public static Lector getInstance(){
+
+    public static Lector getInstance() {
         return instancia;
     }
 
-     public boolean verificarArchivo(final String path){
-        if(path != null && !path.trim().isEmpty()){
+    public boolean verificarArchivo(final String path) {
+        if (path != null && !path.trim().isEmpty()) {
             //Verifica que el path sea un archivo, tenga permisos de lectura y que no sea oculto
-            return (Paths.get(path).toFile().isFile() && Paths.get(path).toFile().canRead() && !Paths.get(path).toFile().isHidden());
+            return (Paths.get(path).toFile().isFile() && Paths.get(path).toFile().canRead()
+                    && !Paths.get(path).toFile().isHidden());
         }
-        
+
         return false;
     }
-    
-    public boolean verificarDirectorio(final String path){
-        if(path != null && !path.trim().isEmpty()){
+
+    public boolean verificarDirectorio(final String path) {
+        if (path != null && !path.trim().isEmpty()) {
             //Verifica que el path sea un directorio, tenga permisos de lectura y que no sea oculto
-            return (Paths.get(path).toFile().isDirectory() && Paths.get(path).toFile().canRead() && !Paths.get(path).toFile().isHidden());
+            return (Paths.get(path).toFile().isDirectory() && Paths.get(path).toFile().canRead()
+                    && !Paths.get(path).toFile().isHidden());
         }
         return false;
     }
-    
-    public List<String> obtenerArchivos(final String path){
+
+    public List<String> obtenerArchivos(final String path) { // metodo encargado de buscar en el path todos los archivos .csv
         List<String> listaArchivos = new ArrayList<>();
-        if(verificarArchivo(path)){
+        if (verificarArchivo(path)) {
             listaArchivos.add(path);
-        }else if(verificarDirectorio(path)){
-            try {           
-            Files.walk(Paths.get(path)).filter(a -> a.toFile().getName().endsWith(".csv")).forEach(p -> listaArchivos.add(p.toString()));
-            listaArchivos.forEach(System.out::println);
-   
+        } else if (verificarDirectorio(path)) {
+            try {
+                Files.walk(Paths.get(path)).filter(a -> a.toFile().getName().endsWith(".csv"))
+                        .forEach(p -> listaArchivos.add(p.toString()));
+                
+                listaArchivos.forEach(System.out::println);
+
             } catch (IOException ex) {
                 Logger.getLogger(Lector.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }      
+        }
         return listaArchivos;
     }
-    
-    public void leerArchivo(List<String> listaArchivos){
-        List<List> cadena = new ArrayList(); //aqui se almacenan los objetos separados por comas de cada linea que contenga el archivo
-        listaArchivos.forEach(l -> {
-            try {
-                Stream<String> stream = Files.lines(Paths.get(l)).skip(1); //se obtiene el flujo de datos y se realiza un salto de linea
-                stream.forEach(a -> cadena.add(separador(a)));
-            } catch (IOException ex) {
-                Logger.getLogger(Lector.class.getName()).log(Level.SEVERE, null, ex);
+
+    // este metodo Genera una lista con los objetos separados por "," de todos los archivos .csv
+    public List<List<String>> parser(final List<String> paths, final boolean saltarLinea, 
+            final String separador) throws IOException {
+        List<List<String>> listado = new ArrayList<>();    
+
+        paths.forEach(p -> {
+            if (verificarArchivo(p)) {
+                try (Stream<String> lines = Files.lines(Paths.get(p))) {
+                    lines.skip(saltarLinea ? 1 : 0).filter(l -> l.contains(separador)).
+                            forEach(s -> {
+                                String[] separado = s.split(separador);
+                                List<String> lista = new ArrayList<>();
+                                for (String string : separado) {
+                                    lista.add(string.trim());
+                                }
+                                listado.add(lista);
+                            });
+                } catch (IOException ex) {
+                    Logger.getLogger(Lector.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
-        cadena.forEach(a -> a.forEach(System.out::println));
-    }
-    
-    public List<String> separador(String linea){
-        String[] separado = linea.split(","); // separa la linea por comas
-        List<String> listaSeparado = new ArrayList<>(Arrays.asList(separado));  //se convierte a una lista 
-        
-        return listaSeparado; 
-    }
-    
-    public List<List<String>> parser(final String path, final boolean saltarLinea, final String separador) throws IOException{
-        List<List<String>> listado=new ArrayList<>();  //Algo asi como que una lista multidimensional     
-        if (verificarArchivo(path)) { //llamamos al metodo validar que creamos
-            try (Stream<String> lines = Files.lines(Paths.get(path))) {
-            lines.skip(saltarLinea?1:0).filter(l->l.contains(separador)).
-                    forEach(s -> {
-                String[] separado = s.split(separador);   
-                listado.add(Arrays.asList(separado));
-                    });
-            return listado;
-            }
-        }
-        
-        return null;
+
+        return listado;
     }
 
 }
--- mantenimiento/src/test/java/ues/edu/sv/tpi135_ingenieria/mantenimiento/LectorTest.java
+++ mantenimiento/src/test/java/ues/edu/sv/tpi135_ingenieria/mantenimiento/LectorTest.java
@@ -6,19 +6,19 @@
 package ues.edu.sv.tpi135_ingenieria.mantenimiento;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.assertArrayEquals;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
 /**
@@ -28,16 +28,11 @@ import org.junit.rules.TemporaryFolder;
 public class LectorTest {
 
     @Rule
-    public TemporaryFolder dirPrueba = new TemporaryFolder();
+    public TemporaryFolder carpetaTemp = new TemporaryFolder();
 
     @Test
     public void crearArchivosTemp() throws IOException {
-        String path;
-        File tempFile = dirPrueba.newFile("prueba.csv");
-        File tempFolder = dirPrueba.newFolder("Folder_prueba");
-        
-        System.out.println(tempFile);
-        System.out.println("Url Folder de prueba" + dirPrueba.getRoot().toString());
+
     }
 
     public LectorTest() {
@@ -60,92 +55,112 @@ public class LectorTest {
     }
 
     /**
-     * Test of leerArchivo method, of class Lector.
+     * Test of getInstance method, of class Lector.
+     *
+     * @Test public void testGetInstance() { System.out.println("getInstance");
+     * Lector expResult = null; Lector result = Lector.getInstance();
+     * assertEquals(expResult, result); // TODO review the generated test code
+     * and remove the default call to fail. // fail("The test case is a
+     * prototype."); }
+     *
+     * /**
+     * Test of verificarArchivo method, of class Lector.
      */
-    
     @Test
-    public void testLeerArchivo() {
-        try {
-            System.out.println("leerArchivo");
-            Lector instance = new Lector();
-            File archivo1 = dirPrueba.newFile("Archivo1.csv");
-            File archivo2 = dirPrueba.newFile("Archivo2.csv");     
-            List<String> archivo = new ArrayList();
-            archivo.add(archivo1.toString());
-            archivo.add(archivo2.toString());
-            instance.leerArchivo(archivo);
-            // TODO review the generated test code and remove the default call to fail.
-        } catch (IOException ex) {
-            Logger.getLogger(LectorTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
-
-    /**
-     * Test of separador method, of class Lector.
-     */
-    
-    @Test
-    public void testSeparador() {
-        System.out.println("separador");
-        String linea = "";
-        Lector instance = new Lector();
-        String[] expResult = null;
-        String[] result = null;
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-
-    }
-       
-    /**
-     * Test of verificarPath method, of class Lector.
-     */
-    //https://junit.org/junit4/javadoc/4.12/org/junit/rules/TemporaryFolder.html 
-    //De aqui saque lo de temporary Folder
-    @Rule
-    public TemporaryFolder carpeta = new TemporaryFolder();
-    
-    @Test
-    public void testVerificarPath() throws IOException {
-        System.out.println("verificarPath");
-        File archivoTemp = carpeta.newFile("arhivoTemp.cvs");
+    public void testVerificarArchivo() throws IOException {
+        System.out.println("verificarArchivo");
+        File archivoTemp = carpetaTemp.newFile("arhivoTemp.cvs");
         String path = archivoTemp.getPath();
         Lector instance = new Lector();
         boolean expResult = true;
         boolean result = instance.verificarArchivo(path);
         assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
     }
-    
+
+    /**
+     * Test of verificarDirectorio method, of class Lector.
+     */
     @Test
-    public void testVerificarPathDirectorio(){
-        System.out.println("Verificar Directorio");
-        String path = carpeta.getRoot().getPath();
+    public void testVerificarDirectorio() {
+        System.out.println("verificarDirectorio");
+        String path = carpetaTemp.getRoot().toString();
         Lector instance = new Lector();
         boolean expResult = true;
         boolean result = instance.verificarDirectorio(path);
         assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
     }
-    
 
     /**
      * Test of obtenerArchivos method, of class Lector.
      */
-    
-    //Marca error de orden de devolucion de archivos
-    
     @Test
     public void testObtenerArchivos() throws IOException {
         System.out.println("obtenerArchivos");
-        File archivo1 = carpeta.newFile("archivo1.csv");
-        File archivo2 = carpeta.newFile("archivo2.csv");
+        File archivo1 = carpetaTemp.newFile("archivo1.csv");
+        File archivo2 = carpetaTemp.newFile("archivo2.csv");
         //File carpeta2 = carpeta.newFolder("recursos");
-        String path = carpeta.getRoot().getPath();
+        String path = carpetaTemp.getRoot().getPath();
         Lector instance = new Lector();
         //Bendito sea stackoverflow :v
-        List<File> expResult = new ArrayList<>();
-        expResult.add(archivo1);
-        expResult.add(archivo2);
+        List<String> expResult = new ArrayList<>();
+        expResult.add(archivo1.getCanonicalPath());
+        expResult.add(archivo2.getCanonicalPath());
         List<String> result = instance.obtenerArchivos(path);
         assertEquals(expResult, result);
     }
+
+    /**
+     * Test of parser method, of class Lector.
+     */
+    @Test
+    public void testParser() throws Exception {
+        System.out.println("Parser");
+        String texto = "id_mantenimiento,historico,n_inventario,marca,n_serie,modelo"
+                + ",responsable,sistema_operativo,version,observaciones\n"
+                + "1,HP1,12040.3334.080.0017,Lenovo,MJVZLKF,ThinkCentre M82"
+                + ",Lic Rina de Zometa,Windows,7.0 Professional SP 64 bits,office pirata";
+
+        String texto2 = "id_mantenimiento,historico,n_inventario,marca,n_serie,modelo"
+                + ",responsble,sistema_operativo,version,observaciones\n "
+                + "2,HP2,12040.3334.080.0008,Dell,551LHY,Vostron 220,Lic Rina de Zometa"
+                + ",Windows,7.0 Professional SP 64 bits,Office Original";
+
+        File tempfile = carpetaTemp.newFile("prueba.csv");
+        File tempfile2 = carpetaTemp.newFile("prueba2.csv");
+
+        try (FileWriter fw = new FileWriter(tempfile.getAbsolutePath());
+                PrintWriter pw = new PrintWriter(fw)) {
+            pw.println(texto);
+        }
+        try (FileWriter fw = new FileWriter(tempfile2.getAbsolutePath());
+                PrintWriter pw = new PrintWriter(fw)) {
+            pw.println(texto2);
+        }
+        String paths = carpetaTemp.getRoot().toString();
+        boolean saltarLinea = true;
+        String separador = ",";
+        Lector instance = new Lector();
+        List<List<String>> expResult = new ArrayList<>();
+        List<String> parse = new ArrayList<>();
+        List<String> parse1 = new ArrayList<>();
+        String[] prueba = {"1", "HP1", "12040.3334.080.0017", "Lenovo", "MJVZLKF",
+             "ThinkCentre M82", "Lic Rina de Zometa","Windows", "7.0 Professional SP 64 bits", "office pirata"};
+        String[] otraPrueba = {"2", "HP2", "12040.3334.080.0008", "Dell", "551LHY",
+            "Vostron 220", "Lic Rina de Zometa",
+            "Windows","7.0 Professional SP 64 bits", "Office Original"};
+        parse.addAll(Arrays.asList(prueba));
+        parse1.addAll(Arrays.asList(otraPrueba));
+        expResult.add(0, parse);
+        expResult.add(1, parse1);
+        List<String> path = instance.obtenerArchivos(paths);
+        List<List<String>> result = instance.parser(path, saltarLinea, separador);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
 }
